af1ca33473ec2c6f850328d85b16588898f3ea1a
Igor <igormasmas1@gmail.com>
2022-07-28 20:44:12 +0200

    28.07

---
+++ MassTransit Starting with Mediator/Controllers/OrderController.cs
@@ -0,0 +1,65 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Contracts;
+
+namespace MassTransit_Starting_with_Mediator.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class OrderController : ControllerBase
+    {
+
+        private readonly ILogger<OrderController> _logger;
+        private readonly IRequestClient<SubmitOrder> _submitOrderRequestClient;
+        private readonly ISendEndpointProvider _sendEndpointProvider;
+
+        public OrderController(ILogger<OrderController> logger, IRequestClient<SubmitOrder> submitOrderRequestClient,ISendEndpointProvider sendEndpointProvider)
+        {
+            _logger = logger;
+            _submitOrderRequestClient = submitOrderRequestClient;
+            _sendEndpointProvider = sendEndpointProvider;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Guid id, string customerNumber)
+        {
+            var (accepted, rejected) = await _submitOrderRequestClient.GetResponse<OrderSubmissionAccepted, OrderSubmissionRejected>(new
+            {
+                OrderId = id,
+                InVar.Timestamp,
+                CustomerNumber = customerNumber
+            });
+            if (accepted.IsCompletedSuccessfully)
+            {
+                var response = await accepted;
+                return Ok(response);
+            }
+
+            else
+            {
+                var response = await rejected;
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Guid id, string customerNumber)
+        {
+            var endpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"exchange:submit-order"));
+
+            await endpoint.Send<SubmitOrder>(new
+            {
+                OrderId = id,
+                InVar.Timestamp,
+                CustomerNumber = customerNumber
+            });
+           
+            return Accepted();
+           
+            
+        }
+    }
+
+}
